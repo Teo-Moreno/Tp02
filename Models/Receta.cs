90b4a816cf3c5eee197a0ef43b3094743c31af66
TEO BENJAMIN MORENO <50291929@ort>
2026-05-12 09:04:35 -0300

    Clase Receta

---
+++ Models/Receta.cs
@@ -0,0 +1,11 @@
+namespace TP01.Models;
+
+public class Receta
+{
+    private string NombreCocinero{get;set;};
+    private date FechaNacimiento{get;set;};
+    private string Tipo{get;set;};
+    private int Precio{get;set;};
+    private int CantCocineros{get;set;};
+
+}

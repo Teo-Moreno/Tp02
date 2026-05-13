fbaa9b5dac7dcc9b61090f7e91582f46a9c46791
TEO BENJAMIN MORENO <50291929@ort>
2026-05-13 08:27:05 -0300

    Formulario en el Index

--- Models/Receta.cs
+++ Models/Receta.cs
@@ -1,11 +1,10 @@
-namespace TP01.Models;
+namespace Tp02.Models;
 
 public class Receta
 {
-    private string NombreCocinero{get;set;};
-    private date FechaNacimiento{get;set;};
-    private string Tipo{get;set;};
-    private int Precio{get;set;};
-    private int CantCocineros{get;set;};
-
+    private string NombreCocinero{get;set;}
+    private DateTime FechaNacimiento{get;set;}
+    private string Tipo{get;set;}
+    private int Precio{get;set;}
+    private int CantCocineros{get;set;}
 }
